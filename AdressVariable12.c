0372a2164185dca5c2ee8b7c3f55cc8e576864a0
Mondira Biswas <biswasmondira57@gmail.com>
2025-02-17 16:14:11 +0600

    Add files via upload

---
+++ AdressVariable12.c
@@ -0,0 +1,18 @@
+/*Write a program to print the address of a variable along with its value*/
+#include<stdio.h>
+int main()
+{
+
+
+char a;
+int x;
+float p,q;
+a='A';
+x=125;
+p=10.25,q=18.76;
+printf("%c is stored at addr %u\n",a,&a);
+printf("%d is stored at addr %u\n",x,&x);
+printf("%f is stored at addr %u\n",p,&p);
+printf("%f is stored at addr %u\n",q,&q);
+return 0;
+}
